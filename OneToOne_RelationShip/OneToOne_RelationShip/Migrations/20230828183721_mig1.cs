72d5d23a7a9f20b4929973a6eeddd61dc15c7c74
Halil Gedik <halilgedik4234@gmail.com>
2023-08-28 21:43:42 +0300

    One To One Relationship Added

---
+++ OneToOne_RelationShip/OneToOne_RelationShip/Migrations/20230828183721_mig1.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OneToOne_RelationShip.Migrations
+{
+    public partial class mig1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Husbands",
+                columns: table => new
+                {
+                    HusbandId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    HusbandName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Age = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Husbands", x => x.HusbandId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Wives",
+                columns: table => new
+                {
+                    WifeId = table.Column<int>(type: "int", nullable: false),
+                    WifeName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Age = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Wives", x => x.WifeId);
+                    table.ForeignKey(
+                        name: "FK_Wives_Husbands_WifeId",
+                        column: x => x.WifeId,
+                        principalTable: "Husbands",
+                        principalColumn: "HusbandId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Wives");
+
+            migrationBuilder.DropTable(
+                name: "Husbands");
+        }
+    }
+}
---
+++ OneToOne_RelationShip/OneToOne_RelationShip/Program.cs
@@ -0,0 +1,187 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+Console.WriteLine("Hello, World!");
+
+#region /* FLUENT API /  One To One Relationship , biraz garip bir örnek ama aklıma anlık bu geldi :) */
+
+//public class Husband
+//{
+//    public int HusbandId { get; set; }
+
+//    public string HusbandName { get; set; }
+
+//    public string Age { get; set; }
+
+
+//    public Wife Wife { get; set; } // navigation property
+//}
+
+//public class Wife
+//{
+//    public int WifeId { get; set; }
+
+//    public string WifeName { get; set; }
+
+//    public string Age { get; set; }
+
+
+//    public Husband Husband { get; set; } // navigation property
+//}
+
+
+
+//public class MarriageContext : DbContext
+//{
+//    public DbSet<Husband> Husbands { get; set; }
+
+//    public DbSet<Wife> Wives { get; set; }
+
+//    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+//    {
+//        optionsBuilder.UseSqlServer("Server=GEDIKPC\\SQLEXPRESS;Database=Relationships;Integrated Security=true;");
+//    }
+
+//    protected override void OnModelCreating(ModelBuilder modelBuilder)
+//    {
+//        modelBuilder.Entity<Husband>()
+//            .HasOne(h => h.Wife)
+//            .WithOne(w => w.Husband)
+//            .HasForeignKey<Wife>(w => w.WifeId);
+
+//        modelBuilder.Entity<Wife>()
+//            .HasKey(w => w.WifeId);
+//    }
+//}
+
+#endregion
+
+#region /*Default Convention*/
+
+//public class Husband
+//{
+//    public int HusbandId { get; set; }
+
+//    public string HusbandName { get; set; }
+
+//    public string Age { get; set; }
+
+
+//    public Wife Wife { get; set; } // navigation property
+//}
+
+//public class Wife
+//{
+//    public int WifeId { get; set; }
+
+//    public string WifeName { get; set; }
+
+//    public string Age { get; set; }
+
+//    public int HusbandId { get; set; }
+//    public Husband Husband { get; set; } // navigation property
+//}
+
+//public class MarriageContext : DbContext
+//{
+//    public DbSet<Husband> Husbands { get; set; }
+
+//    public DbSet<Wife> Wives { get; set; }
+
+//    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+//    {
+//        optionsBuilder.UseSqlServer("Server=GEDIKPC\\SQLEXPRESS;Database=Relationships;Integrated Security=true;");
+//    }
+
+//}
+
+#endregion
+
+#region /*Data Annotations - 1 */
+
+//public class Husband
+//{
+//    public int HusbandId { get; set; }
+
+//    public string HusbandName { get; set; }
+
+//    public string Age { get; set; }
+
+
+//    public Wife Wife { get; set; } // navigation property
+//}
+
+//public class Wife
+//{
+//    public int WifeId { get; set; }
+
+//    public string WifeName { get; set; }
+
+//    public string Age { get; set; }
+
+
+//    [ForeignKey(nameof(Husband))]
+//    public int HusbandId { get; set; }
+//    public Husband Husband { get; set; } // navigation property
+//}
+
+//public class MarriageContext : DbContext
+//{
+//    public DbSet<Husband> Husbands { get; set; }
+
+//    public DbSet<Wife> Wives { get; set; }
+
+//    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+//    {
+//        optionsBuilder.UseSqlServer("Server=GEDIKPC\\SQLEXPRESS;Database=Relationships;Integrated Security=true;");
+//    }
+
+//}
+
+#endregion
+
+#region /*Data Annotations - 2 */
+
+public class Husband
+{
+    public int HusbandId { get; set; }
+
+    public string HusbandName { get; set; }
+
+    public string Age { get; set; }
+
+
+    public Wife Wife { get; set; } // navigation property
+}
+
+public class Wife
+{
+    [Key,ForeignKey(nameof(Husband))]
+    public int WifeId { get; set; }
+
+    public string WifeName { get; set; }
+
+    public string Age { get; set; }
+
+
+
+    public Husband Husband { get; set; } // navigation property
+}
+
+
+public class MarriageContext : DbContext
+{
+    public DbSet<Husband> Husbands { get; set; }
+
+    public DbSet<Wife> Wives { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=GEDIKPC\\SQLEXPRESS;Database=Relationships;Integrated Security=true;");
+    }
+
+}
+
+#endregion
