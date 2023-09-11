16a15bbf0605152503e8414b4af0bb4ffe351f60
Halil Gedik <halilgedik4234@gmail.com>
2023-09-11 13:49:43 +0300

    looking at EagerLoading Include

---
+++ EagerLoading/EagerLoading/Migrations/20230911102620_mig2.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EagerLoading.Migrations
+{
+    public partial class mig2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SupplierCategory",
+                columns: table => new
+                {
+                    SupplierId = table.Column<int>(type: "int", nullable: false),
+                    CategoryId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SupplierCategory", x => new { x.SupplierId, x.CategoryId });
+                    table.ForeignKey(
+                        name: "FK_SupplierCategory_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_SupplierCategory_Suppliers_SupplierId",
+                        column: x => x.SupplierId,
+                        principalTable: "Suppliers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SupplierCategory_CategoryId",
+                table: "SupplierCategory",
+                column: "CategoryId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SupplierCategory");
+        }
+    }
+}
---
+++ EagerLoading/EagerLoading/Program.cs
@@ -0,0 +1,230 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Microsoft.EntityFrameworkCore;
+using System.Reflection.Metadata.Ecma335;
+
+Console.WriteLine("Hello, World!");
+
+ECommerceDbContext db = new();
+#region
+//Product p1 = new() 
+//{
+//    Name = "Saat", 
+//    Price = 1000, 
+//    Quantity = "2", 
+//    Categories = 
+//    { 
+//        new ProductCategory() { Category = new Category() {Name = "Elektronik" } },
+//        new ProductCategory() { Category = new Category() {Name = "Eğlence"}},
+//        new ProductCategory() { Category = new Category() {Name = "Spor"}}
+//    } 
+//};
+
+//Product p2 = new() 
+//{ 
+//    Name = "Bardak", Price = 50, Quantity = "2", Categories = 
+//    { 
+//        new() {Category = new Category(){Name ="Ev Eşyaları" } },  
+//    } 
+//};
+
+//Product p3 = new()
+//{
+//    Name = "Telefon",
+//    Price = 50,
+//    Quantity = "81",
+//    Categories =
+//    {
+//        new() {CategoryId=1},
+//        new() {CategoryId=2}
+//    }
+//};
+
+//Product p4 = new()
+//{
+//    Name = "paten",
+//    Price = 3500,
+//    Quantity = "72",
+//    Categories =
+//    { 
+//        new() {CategoryId=1 },
+//        new() {CategoryId=3},
+//        new() {Category=new Category(){Name="Paten"} }
+//    }
+//};
+
+
+
+//db.Products.AddRange(p1, p2, p3, p4);
+//db.SaveChanges();
+
+//var p = db.Products.Include(p => p.Categories).FirstOrDefault(p => p.Id == 8);
+
+//var cat = db.Categories.ToList();
+
+//foreach (var c in p.Categories)
+//{
+//    Console.WriteLine(c.CategoryId + " " + c.Category.Name);
+//}
+#endregion
+
+#region
+//Supplier s1= new()
+//{
+//    Name = "GBilişim",
+//    SQuantity = 5,
+//    Products = { 
+//        new ProductSupplier(){ProductId = 5},
+//        new ProductSupplier(){ProductId = 7}
+//    }
+//};
+
+//Supplier s2 = new()
+//{
+//    Name = "Skate",
+//    SQuantity = 15,
+//    Products = {
+//        new ProductSupplier(){ProductId = 6},
+//        new ProductSupplier(){ProductId = 8}
+//    }
+//};
+
+//Supplier s3 = new()
+//{
+//    Name = "Ncam",
+//    SQuantity = 121,
+//    Products = {
+//        new ProductSupplier(){ProductId = 6}
+//    }
+//};
+
+//db.Suppliers.AddRange(s1 , s2 , s3);
+//db.SaveChanges();
+
+#endregion
+
+
+public class Product
+{
+    public Product()
+    {
+        Categories = new List<ProductCategory>();
+        Suppliers = new List<ProductSupplier>();
+    }
+
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public float Price { get; set; }
+
+    public string Quantity { get; set; }
+
+
+    public ICollection<ProductCategory> Categories { get; set; }
+
+    public ICollection<ProductSupplier> Suppliers { get; set; }
+}
+
+
+public class Category
+{
+    public Category()
+    {
+        Products = new List<ProductCategory>();
+    }
+
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+
+    public ICollection<ProductCategory> Products { get; set; }
+}
+
+public class Supplier
+{
+    public Supplier()
+    {
+        Products = new List<ProductSupplier>();
+    }
+
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public int SQuantity { get; set; }
+
+
+    public ICollection<ProductSupplier> Products { get; set; }
+}
+
+//cross table
+public class ProductCategory
+{
+    public int ProductId { get; set; }
+
+    public int CategoryId { get; set; }
+
+
+    public Product Product { get; set; }
+    public Category Category { get; set; }
+}
+//cross table
+public class ProductSupplier
+{
+    public int ProductId { get; set; }
+
+    public int SupplierId { get; set; }
+
+
+    public Product Product { get; set; }
+    public Supplier Supplier { get; set; }
+}
+
+public class ECommerceDbContext : DbContext
+{
+    public DbSet<Product> Products { get; set; }
+
+    public DbSet<Category> Categories { get; set; }
+
+    public DbSet<Supplier> Suppliers { get; set; }
+   
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(@"Server=.\SQLEXPRESS;Database=EagerLoadingDb;Integrated Security=True");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<ProductCategory>()
+                    .HasKey(pc => new { pc.ProductId, pc.CategoryId });
+
+        modelBuilder.Entity<ProductCategory>()
+                    .HasOne(pc => pc.Product)
+                    .WithMany(p => p.Categories)
+                    .HasForeignKey(pc => pc.ProductId);
+
+        modelBuilder.Entity<ProductCategory>()
+                    .HasOne(pc => pc.Category)
+                    .WithMany(c => c.Products)
+                    .HasForeignKey(pc => pc.CategoryId);
+
+
+
+        modelBuilder.Entity<ProductSupplier>()
+                    .HasKey(pc => new {pc.ProductId, pc.SupplierId});
+
+        modelBuilder.Entity<ProductSupplier>()
+                    .HasOne(pc => pc.Supplier)
+                    .WithMany(s=>s.Products)
+                    .HasForeignKey(pc=>pc.SupplierId);
+        
+        modelBuilder.Entity<ProductSupplier>()
+                    .HasOne(pc => pc.Product)
+                    .WithMany(p=>p.Suppliers)
+                    .HasForeignKey(pc => pc.ProductId);
+          
+    }
+}
